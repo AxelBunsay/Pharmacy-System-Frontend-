d93eca7cd162e855186a068f32416d5b7db0ecae
Marriel223 <23-17177@g.batstate-u.edu.ph>
2025-12-04 13:57:09 +0800

    Day 3 Dastas

--- src/components/Suppliers.jsx
+++ src/components/Suppliers.jsx
@@ -56,7 +56,7 @@ export default function Suppliers() {
 
                   <button
                     onClick={() => handleCopyContact(s)}
-                    className="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
+                    className="px-3 py-1 bg-green-500 text-white rounded hover:bg-blue-600"
                   >
                     Copy
                   </button>
