cb007ced24cfdeb4cba7e2f2065cb34e13c29ad9
AxelBunsay <akurunogia12@gmail.com>
2025-12-16 07:46:14 +0800

    day 6 axel

--- src/api/reports.js
+++ src/api/reports.js
@@ -16,3 +16,19 @@ export async function fetchReportsDaily(date, page = 1) {
 
   return res.json();
 }
+export async function fetchReportsWeekly(page = 1) {
+  const token = localStorage.getItem("token");
+  if (!token) throw new Error("No token found. Please log in.");
+
+  const res = await fetch(${API_URL}/sales/weekly?page=${page}, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: Bearer ${token},
+    },
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch weekly reports.");
+
+  return res.json();
+}
