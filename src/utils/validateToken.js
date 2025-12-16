ee6f79cbbfe2ed31ba1ca6f004011702f0fa39ee
Marriel223 <23-17177@g.batstate-u.edu.ph>
2025-12-16 08:45:47 +0800

    day 7 - marriel

--- src/utils/validateToken.js
+++ src/utils/validateToken.js
@@ -1,7 +1,6 @@
   export function isTokenValid() {
   const token = localStorage.getItem("token");
   if (!token) return false;
-
   try {
     const payload = JSON.parse(atob(token.split(".")[1]));
     const now = Math.floor(Date.now() / 1000);
